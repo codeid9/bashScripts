3d1bad953025c36020bb29d12da528bbdc3b8416
codeid9 <abhaychhani@gmail.com>
2026-05-16 19:51:34 +0530

    sys_health script update

--- sys_health_check.sh
+++ sys_health_check.sh
@@ -15,3 +15,25 @@ df -h
 echo "---------------------------------------"
 
 uptime
+
+
+
+CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+RAM=$(free | grep Mem | awk '{print ($3/$2) * 100.0}')
+DISK=$(df / | grep / | awk '{print $5}' | sed 's/%//g')
+
+echo "CPU Usage: $CPU%"
+echo "RAM Usage: $RAM%"
+echo "Disk Usage: $DISK%"
+
+if (( $(echo "$CPU > 85" | bc -l) )); then
+    echo "WARNING: CPU usage above 85%"
+fi
+
+if (( $(echo "$RAM > 85" | bc -l) )); then
+    echo "WARNING: RAM usage above 85%"
+fi
+
+if [ "$DISK" -gt 85 ]; then
+    echo "WARNING: Disk usage above 85%"
+fi
